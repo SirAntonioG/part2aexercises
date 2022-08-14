4f0fe9bba0e300131b25e668e137e395b43c55a6
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-13 21:53:33 -0500

    Fullstack part2 section a exercises : 2.2 - 2.3 completed

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Part from "./Part";
 
+const TotalExercises = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <p>
+      <b>Total of {totalExercises} exercises</b>
+    </p>
+  );
+};
+
 const Course = ({ course }) => {
   return (
     <>
@@ -8,6 +17,7 @@ const Course = ({ course }) => {
       {course.parts.map((part) => (
         <Part name={part.name} exercises={part.exercises} key={part.id} />
       ))}
+      <TotalExercises parts={course.parts} />
     </>
   );
 };
